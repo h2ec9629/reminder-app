c4961fae13d35ef380077deb775a6d5f9aa85abf
h2ec9629 <h2ec9629@gmail.com>
2026-08-11 23:04:01 +0900

    投函箱PAT接続状態をヘッダーに表示: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// おじさんリマインダー Service Worker v3.12
-const CACHE_NAME = 'ojisan-reminder-v3.12';
+// おじさんリマインダー Service Worker v3.13
+const CACHE_NAME = 'ojisan-reminder-v3.13';
 const ASSETS = [
   './index.html',
   './style.css',
